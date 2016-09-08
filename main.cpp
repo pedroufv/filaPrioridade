a281cf285e17ae3bafbd7d5d8e343cfaccaa4424
Pedro Augusto <pedro.e@ufv.br>
2016-09-08 10:49:55 -0300

    analise tempo por cada metodo insere e remove

--- main.cpp
+++ main.cpp
@@ -15,40 +15,56 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	clock_t tOrde = clock();
-	FilaPrioridadeOrdenada fpo(1000);
+	FilaPrioridadeOrdenada fpo(10000);
 
-	for(int i = 1000; i >= 1; --i)
+	clock_t tempInsOrde = clock();
+	for(int i = 10000; i >= 1; --i)
 		fpo.insere(i);
+	printf("Ordenada - Insere: %.4fs\n", (double)(clock() - tempInsOrde)/CLOCKS_PER_SEC);
 
-	for(int i = 0; i < 999; ++i) {
+	clock_t tempRemOrde = clock();
+	for(int i = 0; i < 9999; ++i) {
 		//cout << "Remove: " << fpo.verifica() << endl;
 		fpo.remove();
 	}
-	printf("Ordenada: %.4fs\n", (double)(clock() - tOrde)/CLOCKS_PER_SEC);
+	printf("Ordenada - Remove: %.4fs\n", (double)(clock() - tempRemOrde)/CLOCKS_PER_SEC);
 
-	clock_t tDese = clock();
-	FilaPrioridadeDesordenada fpd(1000);
+	FilaPrioridadeDesordenada fpd(10000);
 
-	for(int i = 1000; i >= 1; --i)
+	clock_t tempInsDese = clock();
+	for(int i = 10000; i >= 1; --i)
 		fpd.insere(i);
-	for(int i = 0; i < 999; ++i) {
+	printf("Desordenada - Insere: %.4fs\n", (double)(clock() - tempInsDese)/CLOCKS_PER_SEC);
+
+	clock_t tempRemDese = clock();
+	for(int i = 0; i < 9999; ++i) {
 		//cout << "Remove: " << fpd.verifica() << endl;
 		fpd.remove();
 	}
-	printf("Desordenada: %.4fs\n", (double)(clock() - tDese)/CLOCKS_PER_SEC);
+	printf("Desordenada - Remove: %.4fs\n", (double)(clock() - tempRemDese)/CLOCKS_PER_SEC);
 
-	clock_t tHeap = clock();
-	FilaPrioridadeHeapBinario fphb(1000);
+	FilaPrioridadeHeapBinario fphb(10000);
 
-	for(int i = 1000; i >= 1; --i)
+	clock_t tempInsHeap = clock();
+	for(int i = 10000; i >= 1; --i)
 		fphb.insere(i);
+	printf("HeapBinario - Insere: %.4fs\n", (double)(clock() - tempInsHeap)/CLOCKS_PER_SEC);
 
-	for(int i = 0; i < 1000; ++i) {
+	clock_t tempRemHeap = clock();
+	for(int i = 0; i < 10000; ++i) {
 		//cout << "Verifica: " << fphb.verifica() << endl;
 		fphb.remove();
 	}
-    printf("HeapBinario: %.4fs\n", (double)(clock() - tHeap)/CLOCKS_PER_SEC);
+    printf("HeapBinario - Remove: %.4fs\n", (double)(clock() - tempRemHeap)/CLOCKS_PER_SEC);
 
 	return 0;
 }
+
+/*
+Ordenada - Insere: 384.2424s
+Ordenada - Remove: 0.0001s
+Desordenada - Insere: 0.0001s
+Desordenada - Remove: 0.5054s
+HeapBinario - Insere: 0.0001s
+HeapBinario - Remove: 0.0024s
+*/
